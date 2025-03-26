d15dc023dca2d8da40b38828fb65d708850e8f22
relob17 <145400545+relob17@users.noreply.github.com>
2025-03-26 16:03:39 -0400

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
